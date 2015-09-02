992cabb9deb0f870bd38a3fd74b18c17404cc33b
Benedikt Terhechte <terhechte@gmail.com>
2015-09-02 20:14:11 +0200

    Added configuration options for the generator

--- config.clj
+++ config.clj
@@ -23,17 +23,32 @@
  :atomic-build true
  :emacs "/usr/local/bin/emacs"
  :emacs-config " -q "
+ :emacs-custom-setup "(setup-org-image-gen)"
  :emacs-eval ['(setq user-emacs-directory "resources/emacs/")
               '(setq vc-handled-backends ())
               '(add-to-list 'load-path "~/.emacs.d/org-mode/lisp")
               '(add-to-list 'load-path "~/.emacs.d/org-mode/contrib/lisp")
               '(add-to-list 'load-path "resources/emacs/htmlize-20130207.2102/")
-              ;'(add-to-list 'load-path "resources/emacs/soothe-theme-20130805.1700/")
+              '(add-to-list 'load-path "resources/emacs/org-image-gen/")
               '(require 'htmlize)
               '(require 'org)
               '(require 'ob)
+              '(require 'org-image-gen)
               '(global-font-lock-mode 1)
               '(load-theme 'adwaita)
+              '(setq org-confirm-babel-evaluate nil) ; automatically export code blocks
+               ;; we need to store the base path before opening a file
+              '(setq org-image-gen-base-path (format "%s" default-directory))
+               ;; this is a small phantomjs script that takes a block of
+               ;; code (handed over via stdin) a template name, renders
+               ;; the code out as an image, and saves the image in a given path
+               ;; i.e. render.js
+               '(setq org-image-gen-code-renderer "resources/emacs/org-image-gen/render.js")
+               ;; where to store the generated images in the file system
+               '(setq org-image-image-out-path "resources/public/img-content/")
+               ;; where will the images be once the static is generated, available for the server
+               '(setq org-image-image-srv-path "/img-content/")
+
               ;'(set-face-foreground 'font-lock-string-face "#afafff")
               ;'(set-face-foreground 'font-lock-keyword-face "#ff5f00")
               ;'(set-face-foreground 'font-lock-function-name-face "#d7af00")
